e0f78215d493397ccb53204b0c74ae22e14fe478
Sebastian Ariel Sdrubolini <sevsdrubolini@gmail.com>
2025-06-15 00:01:09 -0300

    feat: cabmios de servicios de servicio con implementation

--- src/main/java/com/sistematurnos/service/implementation/ServicioService.java
+++ src/main/java/com/sistematurnos/service/implementation/ServicioService.java
@@ -1,18 +1,21 @@
-package com.sistematurnos.service;
-
-import com.sistematurnos.entity.Servicio;
-import com.sistematurnos.repository.IServicioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+package com.sistematurnos.service.implementation;
 
 import java.util.List;
 
-@Service
-public class ServicioService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.sistematurnos.entity.Servicio;
+import com.sistematurnos.repository.IServicioRepository;
+import com.sistematurnos.service.IServicioService;
+
+@Service
+public class ServicioService implements IServicioService{
+    
     @Autowired
     private IServicioRepository servicioRepository;
 
+    @Override
     public Servicio altaServicio(String nombreServicio, int duracion) {
         if (servicioRepository.findByNombreServicio(nombreServicio).isPresent()) {
             throw new IllegalArgumentException("Este Servicio ya existe.");
@@ -21,6 +24,7 @@ public class ServicioService {
         return servicioRepository.save(s);
     }
 
+    @Override
     public Servicio altaServicio(Servicio servicio) {
         if (servicioRepository.findByNombreServicio(servicio.getNombreServicio()).isPresent()) {
             throw new IllegalArgumentException("Este Servicio ya existe.");
@@ -28,22 +32,26 @@ public class ServicioService {
         return servicioRepository.save(servicio);
     }
 
+    @Override
     public Servicio obtenerServicioPorNombre(String nombreServicio) {
         return servicioRepository.findByNombreServicio(nombreServicio)
                 .orElseThrow(() -> new IllegalArgumentException("ERROR: No existe servicio con Nombre: " + nombreServicio));
     }
 
+    @Override
     public Servicio obtenerServicioPorId(int id) {
         return servicioRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("ERROR: No existe servicio con ID: " + id));
     }
 
+    @Override
     public void bajaServicio(int id) {
         Servicio servicio = obtenerServicioPorId(id);
         servicio.setEstado(false);
         servicioRepository.save(servicio);
     }
 
+    @Override
     public Servicio modificarServicio(Servicio servicio) {
         Servicio actual = obtenerServicioPorId(servicio.getId());
         actual.setNombreServicio(servicio.getNombreServicio());
@@ -51,9 +59,9 @@ public class ServicioService {
         return servicioRepository.save(actual);
     }
 
+    @Override
     public List<Servicio> traerServicios() {
         return servicioRepository.findByEstadoTrue();
     }
-
 
 }
