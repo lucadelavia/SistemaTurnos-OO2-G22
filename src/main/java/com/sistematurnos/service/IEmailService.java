b7fbdec24f70dadf838a443299b5df443757bd1b
ValentinaMohamed <mohamedvalentinab@gmail.com>
2025-06-15 19:17:55 -0300

    Agregada implementación para Email

---
+++ src/main/java/com/sistematurnos/service/IEmailService.java
@@ -0,0 +1,8 @@
+package com.sistematurnos.service;
+
+import java.util.Map;
+
+public interface IEmailService {
+
+    public void enviarEmailConHtml(String para, String asunto, String nombreTemplate, Map<String, Object> variables);
+}
